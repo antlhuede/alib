a8f253e3dd7f6a010e0c09472dfdecb844be9af5
Anthony Lhuede <a.lhuede@gmail.com>
2013-12-12 05:36:13 -0800

    Fixed projects so that includes dont conflict with global namespace

--- libs/test/source/test.cpp
+++ libs/test/source/test.cpp
@@ -1,18 +1,40 @@
-#include "test.h"
-#include <iostream>
+#if 0
+#include "alib.h"
 
+#include "core.h"
+#include "debug.h"
+#include "factory.h"
+#include "graphics.h"
+#include "math.h"
+#include "meta.h"
+#include "system.h"
+
+
+#endif
+#include <iostream>
+#include "test.h"
 namespace alib 
 {
-namespace test 
-{
-  void test_func() 
+  namespace test 
   {
-    std::cout << "test test" << std::endl;
+    void test_func() 
+    {
+      //std::cout << "test test" << std::endl;
+    }
   }
 }
-}
-
 int main(void)
 {
+#if 0
+  alib::test_func();
+  alib::core::test_func();
+  alib::debug::test_func();
+  alib::factory::test_func();
+  alib::graphics::test_func();
+  alib::math::test_func();
+  alib::meta::test_func();
+  alib::system::test_func();
+#endif
+  alib::test::test_func();
   return 0;
 }
